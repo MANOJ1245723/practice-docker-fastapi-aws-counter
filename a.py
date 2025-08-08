51b26908cc54dec398151460b66504a0bb9eb6ad
MANOJ1245723 <116479036+MANOJ1245723@users.noreply.github.com>
2025-08-08 13:37:00 +0530

    first

---
+++ a.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import redis
+app = FastAPI()
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
+@app.get('/')
+def get_data():
+    r.incr('visited')
+    value = r.get('visited')
+    return {'visited': int(value)}
